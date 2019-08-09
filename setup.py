2651c3a8927441388b8c6bb2ca4a390a313c4160
Mengdie Kong <mengdie.kong@lixinchuxing.com>
2019-08-09 16:13:06 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="github_api_tester",
+    version="0.1",
+    author_email="kongm@tcd.ie",
+    description="this is just for test purpose",
+    packages=['githubAPITester']
+)
